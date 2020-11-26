32d737edbcd51eaf414d6c10bceda110ca82fc34
spidey52 <satyamkumar5254@gmail.com>
2020-11-26 12:23:04 +0530

    fixed some issue of deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React,{useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 // react - router
 
-import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom"
+import {BrowserRouter as Router, Switch,Route} from "react-router-dom"
 
 import {ToastContainer} from "react-toastify"
 import "react-toastify/dist/ReactToastify.min.css"
--- src/component/Repos.js
+++ src/component/Repos.js
@@ -1,37 +1,29 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import {ListGroup,ListGroupItem} from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
+const Repo = ({ repos_url }) => {
+  const [repos, setRepos] = useState([]);
 
-const Repo = ({repos_url}) => {
-
-    const [repos,setRepos] =  useState([]);
-
+  useEffect(() => {
     const fetchRepo = async () => {
-        const {data} = await Axios.get(repos_url)
+      const { data } = await Axios.get(repos_url);
 
-        setRepos(data);
-    }
-
-useEffect(() => {
-    fetchRepo()
-},[repos_url])
-    return(
-        <ListGroup>
-            {repos.map(repo => (
-                <ListGroupItem key={repo.id}>
-                    
-                    <div className="text-primary">{repo.name}</div>
-                    <div className="text-primary">{repo.language}</div>
-                    <div className="text-primary">{repo.description}</div>
-                    
-
-
-                </ListGroupItem>
-            ))}
-        </ListGroup>
-    )
-
-}
+      setRepos(data);
+    };
+    fetchRepo();
+  }, [repos_url ]);
+  return (
+    <ListGroup>
+      {repos.map((repo) => (
+        <ListGroupItem key={repo.id}>
+          <div className="text-primary">{repo.name}</div>
+          <div className="text-primary">{repo.language}</div>
+          <div className="text-primary">{repo.description}</div>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+};
 
 export default Repo;
